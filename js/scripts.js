cebe898ba013f0a584251798dbde7829b4beef0a
Hernan verar <hernanverar@gmail.com>
2023-01-10 09:37:40 -0800

    created basic conversoin app using js
    co-authored-by: Zachary Cipolletta
    <ZackCipolletta@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+function ozToPound(orignal) {
+    converted = orignal * 0.0625;
+    let originalMeasure = orignal;
+    return originalMeasure + " ounces is " + converted + " pounds of rice.";
+}
+
+function poundToOz(orignal) {
+  converted = orignal * 16;
+    let originalMeasure = orignal;
+    return originalMeasure + " pounds is " + converted + " ounces of rice.";
+}
+
+function ozToGram(orignal) {
+  converted = orignal * 28.3495;
+  let originalMeasure = orignal;
+  return originalMeasure + " ounces is " + converted + " grams of rice.";
+}
+
+function poundToGram(orignal) {
+  converted = orignal * 453.592;
+  let originalMeasure = orignal;
+  return originalMeasure + " pounds is " + converted + " grams of rice.";
+}
+
+function tspToTbsp(orignal) {
+  converted = orignal * 0.333333;
+  let originalMeasure = orignal;
+  return originalMeasure + " teaspons is " + converted + " tablespoons of water.";
+}
+
+function tspToMls(orignal) {
+  converted = orignal * 4.92892;
+  let originalMeasure = orignal;
+  return originalMeasure + " teaspons is " + converted + " milliliters of water.";
+}
+
+function tbsToMls(orignal) {
+  converted = orignal * 14.7868;
+  let originalMeasure = orignal;
+  return originalMeasure + " tabllespoons is " + converted + " milliliters of water.";
+}
+
+
+// function poundToOunce(pound) {
+//     ounce = pound * 0.0625;
+//     let pound = pounds;
+//     return pounds + "pounds" is " +  + " pounds of rice.";
+// }
+
+// A function that converts teaspoons (tsp), tablespoons (tbsp), and cups to milliliters (ml).
